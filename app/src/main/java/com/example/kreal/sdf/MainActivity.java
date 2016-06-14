1810142e65307fb63ab63d9508664014c088b15e
CC <ltee12@hotmail.com>
2016-06-15 02:42:09 +0800

    f
    sf

--- app/src/main/java/com/example/kreal/sdf/MainActivity.java
+++ app/src/main/java/com/example/kreal/sdf/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,14 +18,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findPref(R.id.button);
-        Intent intent = new Intent("org.kreal.FtpServer.ACTION_START_FTPSERVER");
-        sendBroadcast(intent);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("sdf","hhhh");
-                Intent intent = new Intent("org.kreal.FtpServer.ACTION_START_FTPSERVER");
-                sendBroadcast(intent);
+                Log.i("sdf", Environment.getExternalStorageDirectory().getAbsolutePath());
             }
         });
     }
