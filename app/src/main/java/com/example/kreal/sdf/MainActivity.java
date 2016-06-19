a7b2028ffd461b48afc4049944605f1a02c62f37
CC <ltee12@hotmail.com>
2016-06-19 22:54:45 +0800

    s

--- app/src/main/java/com/example/kreal/sdf/MainActivity.java
+++ app/src/main/java/com/example/kreal/sdf/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 
 import java.io.File;
+import java.io.IOException;
 
 
 public class MainActivity extends Activity {
@@ -30,18 +31,42 @@ public class MainActivity extends Activity {
                         Log.i("sdf",url);
                     }
                 };
-                ReDecodeThread.encode(BitmapFactory.decodeFile("/sdcard/123.png"),reDecodeThreadResult);
-//                Log.i("sdf", "2");
-//                File[] files = getExternalFilesDirs(Environment.DIRECTORY_DOWNLOADS);
-//                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-//                startActivity(intent);
-//                for(File file:files){
-//                    Log.i("sdf", file.getAbsolutePath());
-//                }
-//                getApplicationContext().getExternalFilesDir(null);
+                File file1 = new File("/storage/9016-4EF8/sd.txt");
+                try {
+                    file1.createNewFile();
+                    Log.i("sdf","ss");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.i("sdf","fail");
+                }
+                Log.i("sdf", "2");
+                File[] files = getExternalFilesDirs(null);
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+                startActivityForResult(intent,1);
+                for(File file:files){
+                    Log.i("sdf", file.getAbsolutePath());
+                }
+                getApplicationContext().getExternalFilesDir(null);
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            Log.i("sdf",data.getData().toString());
+            File file1 = new File("/storage/9016-4EF8/sd.txt");
+            try {
+                file1.createNewFile();
+                Log.i("sdf","ss");
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.i("sdf","fail");
+            }
+        }
+    }
+
     protected <T> T findPref(int key) {
         return (T) this.findViewById(key);
     }
